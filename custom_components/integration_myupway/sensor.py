80d68ceb21409eb82eac6280cd11d44bb2247dcc
Andreas Kruger <ak@patientsky.com>
2022-10-11 12:51:11 +0200

    More updates

--- custom_components/integration_myupway/sensor.py
+++ custom_components/integration_myupway/sensor.py
@@ -14,27 +14,6 @@ async def async_setup_entry(hass, entry, async_add_devices):
 class IntegrationMyUpwaySensor(IntegrationMyUpwayEntity, SensorEntity):
     """MyUpway Sensor class."""
 
-    def __init__(self, hass, config):
-        """Initialize the sensor."""
-        self._hass = hass
-        self._hass.custom_attributes = {}
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of the sensor."""
-        return self._hass.custom_attributes
-
-    def update(self):
-        attributes = {}
-        attributes['mac'] = 'some data'
-        attributes['sn'] = 'some other data'
-        self._hass.custom_attributes = attributes
-
-    @property
-    def unique_id(self):
-        """Return the unique id."""
-        return f"{DEFAULT_NAME}_{SENSOR}"
-
     @property
     def name(self):
         """Return the name of the sensor."""
